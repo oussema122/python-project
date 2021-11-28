dfbd49a77b0bd38c67abb76ac27f064015e0a83e
oussema <80161665+oussema122@users.noreply.github.com>
2021-11-28 14:38:56 +0100

    Update Password_Managers.py

--- Password_Managers.py
+++ Password_Managers.py
@@ -1,6 +1,6 @@
 pwd = input("What is the master password?")
 quit = "True"
-while not quit:
+while quit:
     mode = input("Would you like to add new password or view exsisting one (view, add) if you want to quit choose q")
     if mode == "q":
         quit = "False"
