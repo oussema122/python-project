c5f593b11f97b3bfd6c971591eeaa981b3321a4e
oussema <80161665+oussema122@users.noreply.github.com>
2021-11-28 17:06:24 +0100

    Update Password_Managers.py

--- Password_Managers.py
+++ Password_Managers.py
@@ -1,4 +1,19 @@
 wd = input("What is the master password?")
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
 quit = "False"
 while not quit:
     mode = input("Would you like to add new password or view exsisting one (view, add) if you want to quit choose q : \n")
