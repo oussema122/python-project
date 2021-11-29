876e62f65384307b4f89aed03eddbb49c9b53047
oussema <80161665+oussema122@users.noreply.github.com>
2021-11-29 21:40:59 +0100

    Update Password_Managers.py

--- Password_Managers.py
+++ Password_Managers.py
@@ -18,7 +18,71 @@ key = load_key()
 fer = Fernet(key)
 def add():
     name = input('Account Name: ')
-    
+    from cryptography.fernet import Fernet
+
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+def add():
+    name = input('Account Name: ')
+    from cryptography.fernet import Fernet
+
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+quit = "False"
+while not quit:
+    mode = input("Would you like to add new password or view exsisting one (view, add) if you want to quit choose q : \n")
+    if mode == "a":
+        quit = "True"
+    if mode == "view":
+        pass 
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode. \n")
+        continue
+quit = "False"
+while not quit:
+    mode = input("Would you like to add new password or view exsisting one (view, add) if you want to quit choose q : \n")
+    if mode == "a":
+        quit = "True"
+    if mode == "view":
+        pass 
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode. \n")
+        continue
 quit = "False"
 while not quit:
     mode = input("Would you like to add new password or view exsisting one (view, add) if you want to quit choose q : \n")
