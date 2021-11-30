3db423e83200615fd4d601d5cd53ae38514a4c5c
oussema <80161665+oussema122@users.noreply.github.com>
2021-11-30 18:44:25 +0100

    Update Password_Managers.py

--- Password_Managers.py
+++ Password_Managers.py
@@ -38,26 +38,6 @@ key = load_key()
 fer = Fernet(key)
 def add():
     name = input('Account Name: ')
-    from cryptography.fernet import Fernet
-
-
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-
-
-def load_key():
-    file = open("key.key", "rb")
-    key = file.read()
-    file.close()
-    return key
-
-
-key = load_key()
-fer = Fernet(key)
-def add():
-    name = input('Account Name: ')
     pwd = input("Password: ")
 quit = "False"
 while not quit:
